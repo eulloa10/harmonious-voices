baa297c240b21c65facc55688d54529371579a2f
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2022-10-25 21:14:20 -0700

    updated seeders

--- app/seeds/messages.py
+++ app/seeds/messages.py
@@ -4,15 +4,15 @@ from app.models import db, Message
 # Adds a demo user, you can add other users here if you want
 def seed_messages():
     demo_message_1 = Message(
-        user_id=1, channel_id='demo@aa.io', content='test message 1')
+        user_id=1, channel_id=1, content='test message 1')
     demo_message_2 = Message(
-        user_id=2, channel_id='demo@aa.io', content='test message 2')
+        user_id=2, channel_id=2, content='test message 2')
     demo_message_3 = Message(
-        user_id=3, channel_id='demo@aa.io', content='test message 3')
+        user_id=3, channel_id=3, content='test message 3')
     demo_message_4 = Message(
-        user_id=3, channel_id='demo@aa.io', content='test message 4')
+        user_id=3, channel_id=4, content='test message 4')
     demo_message_5 = Message(
-        user_id=3, channel_id='demo@aa.io', content='test message 5')
+        user_id=3, channel_id=5, content='test message 5')
 
     db.session.add(demo_message_1)
     db.session.add(demo_message_2)
@@ -30,5 +30,5 @@ def seed_messages():
 # resets the auto incrementing primary key, CASCADE deletes any
 # dependent entities
 def undo_messages():
-    db.session.execute('TRUNCATE messages RESTART IDENTITY CASCADE;')
+    db.session.execute('DELETE FROM messages;')
     db.session.commit()
