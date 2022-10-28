e27adc0c27cc58b36b1ddd126b0ae6ce709b17ed
Sungmin Lee <sungminlee417@gmail.com>
2022-10-27 18:53:31 -0700

    fixed error in users model

--- app/models/user.py
+++ app/models/user.py
@@ -34,5 +34,5 @@ class User(db.Model, UserMixin):
             'id': self.id,
             'username': self.username,
             'email': self.email,
-            'servers': self.servers
+            'servers': [server.to_dict() for server in self.servers]
         }
