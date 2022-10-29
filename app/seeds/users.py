dd81483d6e1774e947374034abb58e74af922623
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2022-10-29 15:30:12 -0700

    testing

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -53,6 +53,6 @@ def seed_users():
 # resets the auto incrementing primary key, CASCADE deletes any
 # dependent entities
 def undo_users():
-    # db.session.execute('TRUNCATE users RESTART IDENTITY CASCADE;')
+    db.session.execute('TRUNCATE users RESTART IDENTITY CASCADE;')
     db.session.execute('DELETE FROM users;')
     db.session.commit()
