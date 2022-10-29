d2ed23870d983184a9358e6baee269146f37dbc1
Andrew Parks <andrew.parks@tutanota.com>
2022-10-29 15:42:13 -0700

    changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Switch, useHistory } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import LoginForm from "./components/auth/LoginForm";
 import SignUpForm from "./components/auth/SignUpForm/SignUpForm";
 import NavBar from "./components/SplashPage/NavBar/NavBar";
@@ -12,15 +12,17 @@ import { authenticate } from "./store/session";
 import ListOwnedServers from "./components/Server/ListOwnedServers";
 import SplashPage from "./components/SplashPage";
 import MeTest from "./components/MeTest/MeTest.js";
-import Channels from "./components/Channels";
 import DirectMessaging from "./components/DirectMessaging";
 import ListAllServers from "./components/Server/ListAllServers";
 import AllServers from "./components/Server/AllServers";
+import Channels from "./components/Channels";
+
 
 function App() {
   const [loaded, setLoaded] = useState(false);
   const dispatch = useDispatch();
   const history = useHistory();
+  const cur = useSelector(state => state.session.user)
 
   useEffect(() => {
     (async () => {
@@ -34,47 +36,84 @@ function App() {
   }
 
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/explore/servers">
-          <ListAllServers />
-        </Route>
-        <Route path="/login">
-          <LoginForm />
-        </Route>
-        <Route path="/me">
-          <ListOwnedServers />
-        </Route>
-        <Route path="/channels/me">
-          <DirectMessaging />
-        </Route>
-        <Route path="/:serverId/channels">
-          <Channels />
-        </Route>
-        <Route path="/servers">
-          <ListAllServers />
-        </Route>
-        <Route exact path="/channels/:channelId/messages">
-          <MessageList />
-        </Route>
-        <Route path="/">
-          <SplashPage />
-        </Route>
-      </Switch>
-      {/* <Route path="/servers">
-          <ListOwnedServers></ListOwnedServers>
-        </Route>
-        <ProtectedRoute path="/users" exact={true}>
-          <UsersList />
-        </ProtectedRoute>
-        <ProtectedRoute path="/users/:userId" exact={true}>
-          <User />
-        </ProtectedRoute>
-        <ProtectedRoute path="/" exact={true}>
-          <h1>My Home Page</h1>
-        </ProtectedRoute>
-      </Switch> */}
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        {loaded && cur ? (
+          <Switch>
+            <Route path='/servers/:serverId/:channelId'>
+              <ListOwnedServers/>
+              <Channels/>
+              <MessageList/>
+            </Route>
+            <Route path='/servers/:serverId'>
+              <ListOwnedServers/>
+              <Channels/>
+            </Route>
+            <Route path='/servers'>
+            <ListOwnedServers/>
+              <AllServers/>
+            </Route>
+            <Route>
+              <ListOwnedServers/>
+            </Route>
+          </Switch>
+        ):(
+          <>
+            <Route exact path='/'>
+              <SplashPage></SplashPage>
+            </Route>
+            <Route path="/login">
+              <LoginForm />
+            </Route>
+            <Route path="/signup">
+              <SignUpForm/>
+            </Route>
+          </>
+        )}
+      </BrowserRouter>
+    </>
+
+    // // <BrowserRouter>
+    //   {/* <Switch>
+    //     <Route path="/explore/servers">
+    //       <ListAllServers />
+    //     </Route>
+    //     <Route path="/login">
+    //       <LoginForm />
+    //     </Route>
+    //     <Route path="/me">
+    //       <ListOwnedServers />
+    //     </Route>
+    //     <Route path="/channels/me">
+    //       <DirectMessaging />
+    //     </Route>
+    //     <Route path="/:serverId/channels">
+    //       <Channels />
+    //     </Route>
+    //     <Route path="/servers">
+    //       <ListAllServers />
+    //     </Route>
+    //     <Route exact path="/channels/:channelId/messages">
+    //       <MessageList />
+    //     </Route>
+    //     <Route path="/">
+    //       <SplashPage />
+    //     </Route>
+    //   </Switch> */}
+    //   {/* <Route path="/servers">
+    //       <ListOwnedServers></ListOwnedServers>
+    //     </Route>
+    //     <ProtectedRoute path="/users" exact={true}>
+    //       <UsersList />
+    //     </ProtectedRoute>
+    //     <ProtectedRoute path="/users/:userId" exact={true}>
+    //       <User />
+    //     </ProtectedRoute>
+    //     <ProtectedRoute path="/" exact={true}>
+    //       <h1>My Home Page</h1>
+    //     </ProtectedRoute>
+    //   </Switch> */}
+    // {/* </BrowserRouter> */}
   );
 }
 
