8a3618c4acf58b7c052d73a75f8a1a5d60d84f08
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2022-11-04 01:15:51 -0700

    updated init.py

--- app/__init__.py
+++ app/__init__.py
@@ -47,31 +47,28 @@ Migrate(app, db)
 # Application Security
 CORS(app)
 
-print("----------------------OSENV", os.environ)
+if os.environ.get("FLASK_ENV") == "production":
+    origins = [
+        "http://harmonious-voices.herokuapp.com",
+        "https://harmonious-voices.herokuapp.com"
+    ]
+else:
+    origins = "*"
 
 # Since we are deploying with Docker and Flask,
 # we won't be using a buildpack when we deploy to Heroku.
 # Therefore, we need to make sure that in production any
 # request made over http is redirected to https.
 # Well.........
-# @app.before_request
-# def https_redirect():
-#     if os.environ.get('FLASK_ENV') == 'production':
-#         if request.headers.get('X-Forwarded-Proto') == 'http':
-#             url = request.url.replace('http://', 'https://', 1)
-#             code = 301
-#             return redirect(url, code=code)
-print("___________OSENV", os.environ.get('FLASK_ENV'))
-
 @app.before_request
 def https_redirect():
-    if os.environ.get("FLASK_ENV") == "production":
-        origins = [
-            "http://harmonious-voices.herokuapp.com",
-            "https://harmonious-voices.herokuapp.com"
-        ]
-    else:
-        origins = "*"
+    if os.environ.get('FLASK_ENV') == 'production':
+        if request.headers.get('X-Forwarded-Proto') == 'http':
+            url = request.url.replace('http://', 'https://', 1)
+            code = 301
+            return redirect(url, code=code)
+print("___________OSENV", os.environ.get('FLASK_ENV'))
+
 
 @app.after_request
 def inject_csrf_token(response):
