d3b8034ec6ae7cd7d2f8f616453e8a54434fcf8e
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2022-11-05 11:33:02 -0700

    removed print statements from init.py

--- app/__init__.py
+++ app/__init__.py
@@ -55,14 +55,10 @@ CORS(app)
 @app.before_request
 def https_redirect():
     if os.environ.get('FLASK_ENV') == 'production':
-        print("------------requestheaders", request.headers)
         if request.headers.get('X-Forwarded-Proto') == 'http':
             url = request.url.replace('http://', 'https://', 1)
             code = 301
             return redirect(url, code=code)
-
-print("___________OSENV", os.environ.get('FLASK_ENV'))
-print("___________XFORWARDEDPROTO", os.environ.get('X-Forwarded-Proto'))
 
 
 @app.after_request
